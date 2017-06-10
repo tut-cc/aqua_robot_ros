765ee01efc6bb391435eca5f3ebf851835358905
Hironori Fujimoto <broadbarredfirefish@gmail.com>
2017-06-10 11:40:58 +0900

    master pc側のROSネットワーク設定を追加
    manual_controller.launchの動作は確認済み

---
+++ aqua_robot_manual_controller/launch/env-loader/master_pc_set_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set ROS Environmet
+source /opt/ros/kinetic/setup.bash
+source /home/fujimoto/catkin_ws/devel/setup.bash
+
+export ROS_IP=192.168.1.254
+export ROS_MASTER_URI=http://192.168.1.254:11311
