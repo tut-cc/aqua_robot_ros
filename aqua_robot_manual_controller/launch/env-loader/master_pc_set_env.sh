1bcf64340ec078c852e285fac59bbb80e8d8c5e3
Hironori Fujimoto <broadbarredfirefish@gmail.com>
2017-07-09 14:55:27 +0900

    PC側のアドレスを変更

--- aqua_robot_manual_controller/launch/env-loader/master_pc_set_env.sh
+++ aqua_robot_manual_controller/launch/env-loader/master_pc_set_env.sh
@@ -4,5 +4,5 @@
 source /opt/ros/kinetic/setup.bash
 source /home/fujimoto/catkin_ws/devel/setup.bash
 
-export ROS_IP=192.168.1.254
-export ROS_MASTER_URI=http://192.168.1.254:11311
+export ROS_IP=192.168.1.1
+export ROS_MASTER_URI=http://192.168.1.1:11311
